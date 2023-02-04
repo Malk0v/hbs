6bd42ed58ed6454016c50d1e8af88b75041ba8bd
Malk0v <106482398+Malk0v@users.noreply.github.com>
2023-02-04 19:40:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import menuTpl from '../templates/menu.hbs';
+//import menuTpl from '../templates/menu.hbs';
 import menuItems from '../js/menu.json';
 
 //==== dark theme ====//
@@ -15,9 +15,32 @@ function changeTheme() {
 
 //==== hbs =====//
 
+const menuMarkup = menuItems.map(({ image, name, price, description, ingredients }) => {
+    return `<li class="menu__item">
+  <article class="card">
+    <img src="${image}" alt="${name}" class="card__image"/>
+    <div class="card__content">
+      <h2 class="card__name">${name}</h2>
+      <p class="card__price">
+        ${price} 
+      </p>
+
+      <p class="card__descr"> ${description}</p>
+
+      <ul class="tag-list">
+        
+        <li class="tag-list__item"> ${ingredients} </li>
+       
+      </ul>
+    </div>
+
+    <button class="card__button button">
+      <i class="material-icons button__icon"> shopping_cart </i>В корзину
+    </button>
+  </article>
+</li>`;}).join('');
+
 const menuRef = document.querySelector(".js-menu");
 
-console.log(menuRef);
-
-const markup = menuTpl(menuItems);
-menuRef.insertAdjacentHTML("beforeend", markup);
+// const markup = menuTpl(menuItems);
+menuRef.insertAdjacentHTML("beforeend", menuMarkup);
